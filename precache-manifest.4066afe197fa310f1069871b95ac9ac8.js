1db0b860baa626166e985fec12f77fe08e1d8ea9
Surajan Shrestha <surajanshrestha35@gmail.com>
2020-09-10 22:23:44 +0545

    Updates

---
+++ precache-manifest.4066afe197fa310f1069871b95ac9ac8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16cb1089b82c88880ed87a50c0266fa4",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "39ab632d32a8395b472b",
+    "url": "/todolist/static/css/main.d48c50b8.chunk.css"
+  },
+  {
+    "revision": "68d48d93d1a35662db94",
+    "url": "/todolist/static/js/2.46647695.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.46647695.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ab632d32a8395b472b",
+    "url": "/todolist/static/js/main.1c93f6eb.chunk.js"
+  },
+  {
+    "revision": "7fe725f5b1335237a4ac",
+    "url": "/todolist/static/js/runtime-main.9bca2d01.js"
+  }
+]);
